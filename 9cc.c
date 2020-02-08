cc3a2b9af913984e4b878dcc2338522c4689d2f8
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2020-02-08 23:31:09 +0900

    update: improve error message

--- 9cc.c
+++ 9cc.c
@@ -31,6 +31,20 @@ struct Token
   char *str;      // トークン文字列
 };
 
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, "");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
 // 現在着目しているトークン
 Token *token;
 
@@ -62,7 +76,7 @@ bool consume(char op)
 void expect(char op)
 {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("'%c'ではありません", op);
+    error_at(token->str, "'%c'ではありません", op);
   token = token->next;
 }
 
@@ -71,7 +85,7 @@ void expect(char op)
 int expect_number(void)
 {
   if (token->kind != TK_NUM)
-    error("'%c'ではありません");
+    error_at(token->str, "数値ではありません");
   int val = token->val;
   token = token->next;
   return val;
@@ -91,9 +105,10 @@ Token *new_token(TokenKind kind, Token *cur, char *str)
   return tok;
 }
 
-// 入力文字列pをトークナイズしてそれを返す
-Token *tokenize(char *p)
+// 入力文字列user_inputをトークナイズしてそれを返す
+Token *tokenize()
 {
+  char *p = user_input;
   Token head;
   head.next = NULL;
   Token *cur = &head;
@@ -120,7 +135,7 @@ Token *tokenize(char *p)
       continue;
     }
 
-    error("トークナイズできません");
+    error_at(p, "トークナイズできません");
   }
 
   new_token(TK_EOF, cur, p);
@@ -135,8 +150,9 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  user_input = argv[1];
   // トークナイズする
-  token = tokenize(argv[1]);
+  token = tokenize();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
