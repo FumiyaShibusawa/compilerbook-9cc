93599b3e2a3492fd68f0317abc9c7eb70e54a591
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2020-02-09 20:25:29 +0900

    update: add parser to do four arithmetic expressions

--- 9cc.c
+++ 9cc.c
@@ -45,6 +45,7 @@ void error_at(char *loc, char *fmt, ...)
   fprintf(stderr, "\n");
   exit(1);
 }
+
 // 現在着目しているトークン
 Token *token;
 
@@ -122,7 +123,7 @@ Token *tokenize()
       continue;
     }
 
-    if (*p == '+' || *p == '-')
+    if (strchr("+-*/()", *p))
     {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
@@ -142,40 +143,149 @@ Token *tokenize()
   return head.next;
 }
 
+// parser
+
+typedef enum
+{
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM  // Integer
+} NodeKind;
+
+typedef struct Node Node;
+struct Node
+{
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+Node *new_node(NodeKind kind)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+};
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs)
+{
+  Node *node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+};
+
+Node *new_num(int val)
+{
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
+
+// NODE: 四則演算は以下の3つの文法で表現できる。これをC関数に落とし込む。
+//       expr    = mul ("+" mul | "-" mul)*
+//       mul     = primary ("*" primary | "/" primary)*
+//       primary = num | "(" expr ")"
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr()
+{
+  Node *node = mul();
+  for (;;)
+  {
+    if (consume('+'))
+      node = new_binary(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_binary(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul()
+{
+  Node *node = primary();
+  for (;;)
+  {
+    if (consume('*'))
+      node = new_binary(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_binary(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+Node *primary()
+{
+  if (consume('('))
+  {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  return new_num(expect_number());
+}
+
+// Assembly code generator
+void gen(Node *node)
+{
+  if (node->kind == ND_NUM)
+  {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind)
+  {
+  case ND_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case ND_SUB:
+    printf("  sub rax, rdi\n");
+    break;
+  case ND_MUL:
+    printf("  imul rax, rdi\n");
+    break;
+  case ND_DIV:
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+    break;
+  default:
+    error("パースできません: node->kind: %d, node->str: %s\n", node->kind, node->val);
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char **argv)
 {
   if (argc != 2)
-  {
-    fprintf(stderr, "引数の個数が正しくありません\n");
-    return 1;
-  }
+    error("%s: 引数の個数が正しくありません\n", argv[0]);
 
   user_input = argv[1];
   // トークナイズする
   token = tokenize();
+  Node *node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  // 式の最初は数でなければならないので、それをチェックして
-  // 最初のmov命令を出力
-  printf("  mov rax, %d\n", expect_number());
-
-  // `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
-  // アセンブリを出力
-  while (!at_eof())
-  {
-    if (consume('+'))
-    {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  gen(node);
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
